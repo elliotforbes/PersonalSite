b802479b81d37bc70738f72934a6613961c45a17
emforce <emforce@live.com>
2016-06-04 15:16:08 +0100

    added the timeline

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,107 @@
-<html>
-    <head>
-        <title>ElliotForbes.co.uk</title>
-        <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('/css/front-end.css') }}">
-    </head>
-    <body>
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>Responsive Vertical Timeline</title>
+</head>
+<body>
+	<header>
+		<h1>Elliot Forbes</h1>
+	</header>
+
+	<section id="cd-timeline" class="cd-container">
+		
+        <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="img/cd-icon-location.svg" alt="location">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2>University of Strathclyde | Upper Second Class in Computer Science</h2>
+				<p>In 2015 I graduated from the University of Strathclyde with an upper second class honours in Computer Science.</p>
+				<a href="#0" class="cd-read-more">Read more</a>
+				<span class="cd-date">September 2011 - May 2015</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
         
-        <h2>Our Nav Goes here</h2>
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="img/cd-icon-location.svg" alt="Location">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2>Barclays Openminds Hackathon Winners</h2>
+				<p>In November of 2013, me and a couple friends participated in the Barclays Openminds Hackathon and managed to secure joint first place with our implementation of a stock market. This ultimately secured me an internship there the following summer.  </p>
+				<a href="#0" class="cd-read-more">Read more</a>
+				<span class="cd-date">November 2013</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="img/cd-icon-location.svg" alt="Location">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2>Barclays Internship</h2>
+				<p>I spent 9 weeks working in the Collateral Optimization Unit team. During this time I worked primarily in SQL with a few weeks spent learning C#. </p>
+				<a href="#0" class="cd-read-more">Read more</a>
+				<span class="cd-date">June 2014-September 2014</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
         
-        @yield('content')    
+        <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="img/cd-icon-location.svg" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2>Freelance Web Developer</h2>
+				<p>During my third year studying I engaged in freelance web development and independently worked with clients in order to fullfil their business visions. My primary focus was ensuring my clients satisfaction, as well as delivering timely and cost-effective websites.</p>
+				<a href="#0" class="cd-read-more">Read more</a>
+				<span class="cd-date">September 2014 - July 2015</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
         
-        <div class="footer">
-            <h2>Our Footer Stuff Goes here</h2>
-        </div>
         
-    </body>
+        <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="img/cd-icon-location.svg" alt="location">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2>Web Developer @ Disabled Access Holidays</h2>
+				<p>Whilst in my final year of studying, I took up a part time job at disabledaccessholidays.com in order to supplement my freelance earnings. My primary job here was to add new content to the site and help improve the site using Search Engine Optimization techniques.</p>
+				<a href="#0" class="cd-read-more">Read more</a>
+				<span class="cd-date">November 2014-February 2015</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="img/cd-icon-location.svg" alt="Picture">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2>Technical Analyst at JPMorgan Chase</h2>
+				<p>Currently I'm working as a Full-Stack developer at JPMorgan Chase in Glasgow. </p>
+				<span class="cd-date">August 2015 - Present.</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+        
+        
+	</section> <!-- cd-timeline -->
+    
+    
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
 </html>
